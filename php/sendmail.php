af6da25eef348edd45f1a955b7682d57df6c58a6
Mateusz Nawrocki <mateusznawrocki@macbook-air-mateusz.home>
2023-09-05 23:34:51 +0200

    last commit, website is done.

--- php/sendmail.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$email = $_POST['email'];
-$msg = $_POST['msg']
-$subject = "Wiadomość wysłana z formularza na stronie JKG Meble";
-$to = "jkgmeble@gmail.com";
-
-$txt = "email: " . $email . "\r\n" . "Treść: " . $msg;
